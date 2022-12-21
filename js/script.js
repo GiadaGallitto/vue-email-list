1881d0ae7d43c7b56e97c81e5a06d4ebc6a24f1b
GiadaGallitto <gallitto.giada@gmail.com>
2022-12-21 15:36:48 +0100

    Add for cycle to add Random Email to list

--- js/script.js
+++ js/script.js
@@ -4,19 +4,23 @@ createApp({
     data(){
         return{
             randomEmail: null,
+            emailList: []
         }
     },
 
     methods: {
-
+        getRandomEmail() {
+        }
     },
-
-    mounted(){
-
-        axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
-            .then((response) => {
-                console.log(response.data.response)
-                this.randomEmail = response.data.response
-            })
+    
+    mounted(){       
+        for (i=0; i < 10; i++) {
+            axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+                .then((response) => {
+                    console.log(response.data.response)
+                    this.randomEmail = response.data.response
+                    this.emailList.push(this.randomEmail)
+                })
+        }
     }
 }).mount(`#app`)
